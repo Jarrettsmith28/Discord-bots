712ce554ba693420fe0fab226208a973b8646648
Jarrettsmith28 <79722509+Jarrettsmith28@users.noreply.github.com>
2021-02-26 13:23:55 -0800

    Add files via upload

---
+++ alex.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json');
+
+const embed = new Discord.MessageEmbed()
+	.setTitle('Some Title')
+	.setColor('#0099ff');
+
+client.once('ready', async () => {
+	const channel = client.channels.cache.get('222197033908436994');
+	try {
+		const webhooks = await channel.fetchWebhooks();
+		const webhook = webhooks.first();
+
+		await webhook.send('Webhook test', {
+			username: 'some-username',
+			avatarURL: 'https://i.imgur.com/wSTFkRM.png',
+			embeds: [embed],
+		});
+	} catch (error) {
+		console.error('Error trying to send: ', error);
+	}
+});
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler =>{
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+client.login('ODEzNDUzOTAyNDE3NjI1MDk4.YDPh-w.hGy6uIuXIJbWkxnUS2MIkDA4GUc');
